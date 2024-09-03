b22bde609735ff54e8ab301f534a34513e3e1ec1
Joel <fitness.joeld@gmail.com>
2024-09-03 00:53:21 -0400

    open api initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[My Open API Project]
+(https://github.com/JGDel24/joeldeleon-open-api)
---
+++ index.js
@@ -0,0 +1,22 @@
+const publicKey = '7aec8be88ac3f8fff8a0ee870fe35702';
+const privateKey = '8ee06740c1ffb4e5e3a4213242522e33130db1d8';
+const ts = Date.now().toString();
+const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+
+const url = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+    console.log(data);
+
+    
+})
+
+.catch(function(error) {
+    console.error('Error fetching data:', error);
+});
+
